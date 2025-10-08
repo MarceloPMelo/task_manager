2e373302fe31a53bcd655aa22bcb418ff9d09de7
MarceloPMelo <mpedrosa.melo@gmail.com>
2025-10-08 10:52:03 -0300

    ordenação

--- front/src/components/ContactTable.tsx
+++ front/src/components/ContactTable.tsx
@@ -15,7 +15,11 @@ import {
   TextField,
   Button,
   FormGroup,
-  FormControlLabel
+  FormControlLabel,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
 } from "@mui/material";
 import type { RootState } from "@/store";
 import { useEffect, useState } from "react";
@@ -30,6 +34,19 @@ type Filters = {
   companies: string[];
   jobTitles: string[];
 };
+
+const sortOptions = [
+  { label: "Name Asc", value: { sortBy: "name", direction: "asc" } },
+  { label: "Name Desc", value: { sortBy: "name", direction: "desc" } },
+  { label: "Company Asc", value: { sortBy: "company", direction: "asc" } },
+  { label: "Company Desc", value: { sortBy: "company", direction: "desc" } },
+  { label: "Job Title Asc", value: { sortBy: "jobTitle", direction: "asc" } },
+  { label: "Job Title Desc", value: { sortBy: "jobTitle", direction: "desc" } },
+  { label: "ID Asc", value: { sortBy: "id", direction: "asc" } },
+  { label: "ID Desc", value: { sortBy: "id", direction: "desc" } },
+  { label: "Email Asc", value: { sortBy: "email", direction: "asc" } },
+  { label: "Email Desc", value: { sortBy: "email", direction: "desc" } },
+];
 
 export function ContactTable() {
 
@@ -132,117 +149,145 @@ export function ContactTable() {
     <Box maxWidth="lg" mx="auto" alignItems="flex-start" px={2} py={4} display="flex" gap={3}>
       {/* Filtros e ordenação */}
       <Box
-      minWidth={260}
-      maxWidth={320}
-      flexShrink={0}
-      display="flex"
-      flexDirection="column"
-      gap={2}
-      component="form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        fetchContacts(1, searchInput);
-        setCurrentPage(1);
-      }}
+        minWidth={260}
+        maxWidth={320}
+        flexShrink={0}
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        component="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchContacts(1, searchInput);
+          setCurrentPage(1);
+        }}
       >
-      <h1>Filtros de Busca</h1>
-      <TextField
-        label="Nome"
-        name="nome"
-        value={searchInput.search}
-        onChange={(e) => setSearchInput({ ...searchInput, search: e.target.value })}
-      />
-      <FormGroup>
-        <Typography variant="subtitle1" gutterBottom>Companies</Typography>
-        {enums.companies.map((company) => (
-          <FormControlLabel
-            key={company}
-            control={
-              <Checkbox value={company}
-                checked={(searchInput.company ?? []).includes(company)}
-                onChange={handleCompany}
-              />
-            }
-            label={company}
-          />
-        ))}
-      </FormGroup>
+        <h1>Filtros de Busca</h1>
+        <TextField
+          label="Nome"
+          name="nome"
+          value={searchInput.search}
+          onChange={(e) => setSearchInput({ ...searchInput, search: e.target.value })}
+        />
+        <FormGroup>
+          <Typography variant="subtitle1" gutterBottom>Companies</Typography>
+          {enums.companies.map((company) => (
+            <FormControlLabel
+              key={company}
+              control={
+                <Checkbox value={company}
+                  checked={(searchInput.company ?? []).includes(company)}
+                  onChange={handleCompany}
+                />
+              }
+              label={company}
+            />
+          ))}
+        </FormGroup>
 
-      <FormGroup>
-        <Typography variant="subtitle1" gutterBottom>Job Titles</Typography>
-        {enums.jobTitles.map((jobTitle) => (
-          <FormControlLabel
-            key={jobTitle}
-            control={
-              <Checkbox value={jobTitle}
-                checked={(searchInput.jobTitle ?? []).includes(jobTitle)}
-                onChange={handleJobTitle}
-              />
-            }
-            label={jobTitle}
-          />
-        ))}
-      </FormGroup>
-      <Button type="submit" variant="contained">
-        Buscar
-      </Button>
+        <FormGroup>
+          <Typography variant="subtitle1" gutterBottom>Job Titles</Typography>
+          {enums.jobTitles.map((jobTitle) => (
+            <FormControlLabel
+              key={jobTitle}
+              control={
+                <Checkbox value={jobTitle}
+                  checked={(searchInput.jobTitle ?? []).includes(jobTitle)}
+                  onChange={handleJobTitle}
+                />
+              }
+              label={jobTitle}
+            />
+          ))}
+        </FormGroup>
+
+        <FormGroup>
+          <FormControl>
+            <InputLabel>Sort By</InputLabel>
+            <Select
+              labelId="sort"
+              id="sort"
+              value={searchInput.sortBy && searchInput.direction ? sortOptions.find(option => option.value.sortBy === searchInput.sortBy && option.value.direction === searchInput.direction)?.label || "Clean-Sort" : "Clean-Sort"}
+              label="Sort Options"
+              onChange={(e) => {
+                if (e.target.value === "Clean-Sort") {
+                  setSearchInput({ ...searchInput, sortBy: null, direction: null })
+                }
+
+                const selectedOption = sortOptions.find(option => option.label === e.target.value);
+                if (selectedOption) {
+                  setSearchInput({ ...searchInput, sortBy: selectedOption.value.sortBy, direction: selectedOption.value.direction });
+                }
+              }}
+            >
+              {sortOptions.map((option) => (
+                <MenuItem value={option.label}>{option.label}</MenuItem>
+              ))}
+              <MenuItem value="Clean-Sort">Clean Sort</MenuItem>
+            </Select>
+          </FormControl>
+
+        </FormGroup>
+        <Button type="submit" variant="contained">
+          Buscar
+        </Button>
       </Box>
 
       {/* Tabela de contatos e paginação */}
       <Box flex={1} display="flex" flexDirection="column">
-      <TableContainer sx={{ height: 500 }} component={Paper}>
-        <Table>
-        <TableHead>
-          <TableRow sx={{ backgroundColor: "primary.main", color: "white" }}>
-          <TableCell> <Checkbox /> </TableCell>
-          <TableCell>Name</TableCell>
-          <TableCell>Phone</TableCell>
-          <TableCell>Email</TableCell>
-          <TableCell>Company</TableCell>
-          <TableCell>jobTitle</TableCell>
-          <TableCell>address</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {contacts.length !== 0 && contacts.map((contact, index) => (
-          <TableRow key={index}
-            hover
-            sx={{
-            "&:hover": {
-              backgroundColor: "#f0f0f0",
-            },
-            }}
-          >
-            <TableCell> <Checkbox /></TableCell>
-            <TableCell align="left">{contact.name}</TableCell>
-            <TableCell align="left">{contact.phone}</TableCell>
-            <TableCell align="left">{contact.email}</TableCell>
-            <TableCell align="left">{contact.company}</TableCell>
-            <TableCell align="left">{contact.jobTitle}</TableCell>
-            <TableCell align="left">{contact.address}</TableCell>
-          </TableRow>
-          ))}
-        </TableBody>
-        </Table>
-      </TableContainer>
+        <TableContainer sx={{ height: 500 }} component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow sx={{ backgroundColor: "primary.main", color: "white" }}>
+                <TableCell> <Checkbox /> </TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Company</TableCell>
+                <TableCell>jobTitle</TableCell>
+                <TableCell>address</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {contacts.length !== 0 && contacts.map((contact, index) => (
+                <TableRow key={index}
+                  hover
+                  sx={{
+                    "&:hover": {
+                      backgroundColor: "#f0f0f0",
+                    },
+                  }}
+                >
+                  <TableCell> <Checkbox /></TableCell>
+                  <TableCell align="left">{contact.name}</TableCell>
+                  <TableCell align="left">{contact.phone}</TableCell>
+                  <TableCell align="left">{contact.email}</TableCell>
+                  <TableCell align="left">{contact.company}</TableCell>
+                  <TableCell align="left">{contact.jobTitle}</TableCell>
+                  <TableCell align="left">{contact.address}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
-      {/* Paginação */}
-      {contacts.length > 0 && (
-        <Box display="flex" justifyContent="center" alignItems="center" mb={3} mt={2}>
-        <IconButton onClick={handlePrevPage} disabled={currentPage === 1}>
-          <ChevronLeft />
-        </IconButton>
-        <Typography variant="body2" mx={2}>
-          Página {currentPage} de {totalPages}
-        </Typography>
-        <IconButton
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-        >
-          <ChevronRight />
-        </IconButton>
-        </Box>
-      )}
+        {/* Paginação */}
+        {contacts.length > 0 && (
+          <Box display="flex" justifyContent="center" alignItems="center" mb={3} mt={2}>
+            <IconButton onClick={handlePrevPage} disabled={currentPage === 1}>
+              <ChevronLeft />
+            </IconButton>
+            <Typography variant="body2" mx={2}>
+              Página {currentPage} de {totalPages}
+            </Typography>
+            <IconButton
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              <ChevronRight />
+            </IconButton>
+          </Box>
+        )}
       </Box>
     </Box>
   );
